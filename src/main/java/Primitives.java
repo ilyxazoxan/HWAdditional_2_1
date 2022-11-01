66ea14b1b252a26a6969649972ca498b616987d5
Ilya Danilov <esozap32@mail.ru>
2022-11-01 22:43:14 +0400

    Primitives

--- src/main/java/Primitives.java
+++ src/main/java/Primitives.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Primitives {
+import javax.sound.midi.SysexMessage;
+
+public class Primitives {
+    public static void main (String[] args) {
+        byte a = 1;
+        System.out.println("byte = " + a );
+
+        short b = 22;
+        System.out.println("short = " + b );
+
+        int c = 333333;
+        System.out.println("int = " + c );
+
+        long d = 444444444444L;
+        System.out.println("long = " + d );
+
+        float e = 5.55555f;
+        System.out.println("float = " + e );
+
+        double f = 66.66666;
+        System.out.println("double = " + f );
+
+        char g = 'G';
+        System.out.println("char = " + 'G' );
+
+        boolean i = true;
+        System.out.println("boolean = " + i );
+
+         }
 }
+
+
+
+
